0828449bd9b5c8e7f53d53319e5f2a6d9bbf00b1
Wildan Alfarobi <alfarobi.dev@gmail.com>
2024-06-12 15:55:14 +0700

    Authorization Implemented

--- src/server.js
+++ src/server.js
@@ -87,16 +87,27 @@ const init = async () => {
     const { response } = request;
 
     // penanganan client error secara internal.
-    if (response instanceof ClientError) {
+    if (response instanceof Error) {
+      if (response instanceof ClientError) {
+        const newResponse = h.response({
+          status: 'fail',
+          message: response.message,
+        });
+        console.log(`Server Error => ${response.message}`);
+        newResponse.code(response.statusCode);
+        return newResponse;
+      }
+      if (!response.isServer) {
+        return h.continue;
+      }
       const newResponse = h.response({
-        status: 'fail',
-        message: response.message,
+        status: 'error',
+        message: 'Maaf, terjadi kegagalan pada server kami',
       });
       console.log(`Server Error => ${response.message}`);
-      newResponse.code(response.statusCode);
+      newResponse.code(500);
       return newResponse;
     }
-    console.log(`Server Error => ${response.message}`);
     return h.continue;
   });
   await server.start();
